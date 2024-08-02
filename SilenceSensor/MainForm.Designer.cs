7c079428c17d1e422e5e2935089e1d8fb721ebd8
greg davis <djgrego@djgrego.com>
2024-08-02 09:57:32 -0500

    Add project files.

---
+++ SilenceSensor/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SilenceSensor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            numSensorCount = new NumericUpDown();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numSensorCount).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(numSensorCount);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(81, 57);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Sensors";
+            // 
+            // numSensorCount
+            // 
+            numSensorCount.Location = new Point(6, 22);
+            numSensorCount.Name = "numSensorCount";
+            numSensorCount.Size = new Size(53, 23);
+            numSensorCount.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "MainForm";
+            Text = "Silence Sensor";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numSensorCount).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private NumericUpDown numSensorCount;
+    }
+}
---
+++ SilenceSensor/MainForm.cs
@@ -0,0 +1,10 @@
+namespace SilenceSensor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
